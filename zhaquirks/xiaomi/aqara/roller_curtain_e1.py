088b4f011ee5b49a090b5077b4642d3c8d5ec427
jeverley <46714706+jeverley@users.noreply.github.com>
2025-01-27 13:31:06 +0000

    Hide redundant entities by default
    The AnalogOutput cluster is used to write target position to the device.
    The present_value reported/read provides the current position/starting position when the device is in motion.
    
    Both of these functions are re-mapped into the ZCL WindowCovering cluster, so the number entity created is redundant.
    
    This also pre-emptively hides the MultistateOutput cluster, though there is currently no cluster handler implemented for this.

--- zhaquirks/xiaomi/aqara/roller_curtain_e1.py
+++ zhaquirks/xiaomi/aqara/roller_curtain_e1.py
@@ -326,6 +326,8 @@ class MultistateOutputRollerE1(CustomCluster, MultistateOutput):
     #    .friendly_name(
     #        manufacturer="Aqara", model="Roller Shade Driver E1"
     #    )
+    .prevent_default_entity_creation(endpoint_id=1, cluster_id=AnalogOutput.cluster_id)
+    .prevent_default_entity_creation(endpoint_id=1, cluster_id=MultistateOutput.cluster_id)
     .removes(OnOff.cluster_id)
     .replaces(AnalogOutputRollerE1)
     .replaces(BasicCluster)
